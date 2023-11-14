9efc94f63e0230ede91e897b648150e734f282e0
faridEsnaashari <faride13578642@gmail.com>
2023-11-14 20:20:34 +0330

    padding almost removed in 2xsm mode

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,11 +18,11 @@ export default function RootLayout({ children }: LayoutProps) {
   return (
     <html className={BNazaninFont.className} lang="en" dir="rtl">
       <body>
-        <header className="w-full px-16 pt-16 xsm:px-8 xsm:pt-8">
+        <header className="w-full px-2 px-2 pt-16 2xsm:px-16 xsm:px-8 xsm:pt-8">
           <Header />
         </header>
         <main className="flex h-[calc(100vh-5.5rem)] flex-col justify-between xsm:h-auto xsm:flex-row">
-          <div className="w-full px-16 pt-16 xsm:w-1/2 xsm:px-8 xsm:pt-8">{children}</div>
+          <div className="w-full px-2 pt-16 2xsm:px-16 xsm:w-1/2 xsm:px-8 xsm:pt-8">{children}</div>
           <Aside className="relative -z-50 w-full pt-[23%] xsm:w-1/2 xsm:pt-0" />
         </main>
       </body>
