88c9339cc5350bb4685b3452dec75e0d9ef921a8
faridEsnaashari <faride13578642@gmail.com>
2023-11-14 14:06:14 +0330

    padding added for main content

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({ children }: LayoutProps) {
           <Header />
         </header>
         <main className="flex h-[calc(100vh-5.5rem)] flex-col justify-between xsm:h-auto xsm:flex-row">
-          <div className="w-full xsm:w-1/2">{children}</div>
+          <div className="w-full px-16 pt-16 xsm:w-1/2 xsm:px-8 xsm:pt-8">{children}</div>
           <Aside className="relative -z-50 w-full pt-[23%] xsm:w-1/2 xsm:pt-0" />
         </main>
       </body>
