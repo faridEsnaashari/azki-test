cef008ee415210dae6c391d49a83998aecf88154
faridEsnaashari <faride13578642@gmail.com>
2023-11-15 15:43:10 +0330

    project entities

---
+++ src/common/types/entities.type.ts
@@ -0,0 +1,20 @@
+export type VehicleType = {
+  id: number;
+  title: string;
+  usages: VehicleUsage[];
+};
+
+export type VehicleUsage = {
+  id: number;
+  title: string;
+};
+
+export type InsuranceCompany = {
+  id: number;
+  title: string;
+};
+
+export type ThirdDiscount = {
+  id: number;
+  title: string;
+};
