7e95e8849ac71a2ef0d63ef9fe47ded492e8d79e
faridEsnaashari <faride13578642@gmail.com>
2023-11-15 15:38:00 +0330

    now get username from localhost

--- src/app/components/ProfileAction.component.tsx
+++ src/app/components/ProfileAction.component.tsx
@@ -1,5 +1,11 @@
+"use client";
+
+import { text1 } from "../texts";
+
 function ProfileAction() {
-  return <p>salam</p>;
+  const user = localStorage.getItem("user");
+  const username = user ? JSON.parse(user).name : text1;
+  return <p>{username}</p>;
 }
 
 export default ProfileAction;
