e36991dfaf5c48eea0bbb8673fc98b463425c166
faridEsnaashari <faride13578642@gmail.com>
2023-11-15 13:45:55 +0330

    resutl modal added. json saved to local host

--- src/app/discount-type/page.tsx
+++ src/app/discount-type/page.tsx
@@ -5,6 +5,7 @@ import { Button, RadioButton } from "@/common/components/forms";
 import { buttonText, errorText, text1Placeholder, text2Placeholder } from "./texts";
 import { Option } from "@/common/components/forms/components/radio-button.types";
 import useAPICaller from "@/hooks/use-api-caller.hook";
+import { ResultModal } from "./components";
 
 function Page() {
   const [getThirdDiscounts, result] = useAPICaller().getThirdDiscountsCaller;
@@ -13,6 +14,7 @@ function Page() {
   const [selectedThirdDiscount, setSelectedThirdDiscount] = useState<Option>();
   const [selectedDriverDiscount, setSelectedDriverDiscount] = useState<Option>();
   const [showError, setShowError] = useState<"third" | "driver">();
+  const [showModal, setShowModal] = useState(false);
 
   useEffect(() => getThirdDiscounts(), []);
 
@@ -37,36 +39,41 @@ function Page() {
       return;
     }
 
-    localStorage.setItem("thirdDiscount", selectedThirdDiscount.id + "");
-    localStorage.setItem("driverDiscount", selectedDriverDiscount.id + "");
+    localStorage.setItem("thirdDiscount", JSON.stringify(selectedThirdDiscount));
+    localStorage.setItem("driverDiscount", JSON.stringify(selectedDriverDiscount));
+
+    setShowModal(true);
   };
 
   return (
-    <form className="flex w-full flex-col flex-wrap items-center xsm:flex-row xsm:justify-end" onSubmit={onSubmit}>
-      <RadioButton
-        className="w-full pb-4"
-        options={thirdDiscounts}
-        value={selectedThirdDiscount}
-        onValueChange={setSelectedThirdDiscount}
-        placeholder={text1Placeholder}
-        name="third"
-        errorText={errorText}
-        showError={showError === "third"}
-      />
-      <RadioButton
-        className="w-full pb-4"
-        options={thirdDiscounts}
-        value={selectedDriverDiscount}
-        onValueChange={setSelectedDriverDiscount}
-        placeholder={text2Placeholder}
-        name="driver"
-        errorText={errorText}
-        showError={showError === "driver"}
-      />
-      <Button className="flex w-28 justify-center rounded-full bg-[#25b79b] py-2 text-base font-medium text-white">
-        {buttonText}
-      </Button>
-    </form>
+    <>
+      <form className="flex w-full flex-col flex-wrap items-center xsm:flex-row xsm:justify-end" onSubmit={onSubmit}>
+        <RadioButton
+          className="w-full pb-4"
+          options={thirdDiscounts}
+          value={selectedThirdDiscount}
+          onValueChange={setSelectedThirdDiscount}
+          placeholder={text1Placeholder}
+          name="third"
+          errorText={errorText}
+          showError={showError === "third"}
+        />
+        <RadioButton
+          className="w-full pb-4"
+          options={thirdDiscounts}
+          value={selectedDriverDiscount}
+          onValueChange={setSelectedDriverDiscount}
+          placeholder={text2Placeholder}
+          name="driver"
+          errorText={errorText}
+          showError={showError === "driver"}
+        />
+        <Button className="flex w-28 justify-center rounded-full bg-[#25b79b] py-2 text-base font-medium text-white">
+          {buttonText}
+        </Button>
+      </form>
+      <ResultModal showModal={showModal} setShowModal={setShowModal} />
+    </>
   );
 }
 
