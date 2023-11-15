0730ade1135a8e39942fac769c6ca6d16682905a
faridEsnaashari <faride13578642@gmail.com>
2023-11-15 15:40:11 +0330

    texts for signup errors

--- src/app/sign-up/texts.ts
+++ src/app/sign-up/texts.ts
@@ -4,3 +4,9 @@ export const namePlaceholder = "نام";
 export const familyPlaceholder = "نام خانوادگی";
 export const phonenumberPlaceholder = "شماره موبایل";
 export const passwordPlaceholder = "رمز عبور";
+
+export const errorText1 = "لطفا فیلد را پر کنید";
+export const errorText2 = "شماره موبایل را درست وارد نمایید";
+export const errorText3 = "فقط حروف فارسی مجاز است";
+export const errorText4 =
+  "پسورد باید از حداقل ۴ حرف و حداکثر ۱۰ حرف و حداقل یک عدد و یک حرف لاتین بزرگ و یک حرف لاتین کوچک تشکیل شده باشد";
